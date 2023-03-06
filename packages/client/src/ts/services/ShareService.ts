6dff0b69ea733ae65e3d0f3eb9e04956d57b1750
david-cc <david.caudeli@web-education.net>
2023-03-06 20:04:37 +0100

    feat: #WB2-404, update ShareService types

--- packages/client/src/ts/services/ShareService.ts
+++ packages/client/src/ts/services/ShareService.ts
@@ -1,21 +1,55 @@
-import { ResourceRight, RightRole } from "./RightService";
-
 export class ShareService {
-  constructor() {}
-
-  getRightsForResource(resourceId: string): Promise<ResourceRight[]> {
-    const mockRight: ResourceRight = {
-      type: "user",
+  getRightsForResource(resourceId: string): Promise<ShareRight[]> {
+    const mockRight: ShareRight = {
       id: "right_1",
-      right: "read",
+      type: "user",
+      username: "user 1",
+      name: "user_1",
+      actions: [
+        {
+          id: "1",
+          displayName: "read",
+        },
+      ],
+      hide: false,
     };
     return Promise.resolve([mockRight]);
   }
 
-  saveRights(resourceId: string, rights: ResourceRight[]) {}
+  saveRights(resourceId: string, rights: ShareRight[]) {}
 
-  getRoleForApp(app: string): Promise<RightRole[]> {
-    const mockRightRole = "read";
-    return Promise.resolve([mockRightRole]);
+  getActionsForApp(app: string): Promise<ShareRightAction[]> {
+    const mockShareRightAction: ShareRightAction[] = [
+      {
+        id: "1",
+        displayName: "read",
+        priority: 1,
+      },
+    ];
+    return Promise.resolve(mockShareRightAction);
   }
 }
+
+export interface ShareRight {
+  id: string;
+  type: ShareRightType;
+  hide: boolean;
+  username: string; // if type is user // TODO can we fix this and have displayName instead for both username and name
+  name: string; // if type is group
+  profile?: string; // if type is user
+  actions: ShareRightAction[];
+}
+
+export type ShareRightType = "user" | "group" | "sharebookmark";
+
+export interface ShareRightAction {
+  id: string;
+  displayName: ShareRightActionDisplayName;
+  priority?: number;
+}
+
+export type ShareRightActionDisplayName =
+  | "read"
+  | "contrib"
+  | "manage"
+  | "creator";
