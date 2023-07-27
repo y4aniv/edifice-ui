e268daaa4d26072ba5f9f71743909b016bd6fba0
Clement Creusat <clement.creusat@gmail.com>
2023-07-27 11:11:40 +0200

    feat: add mindmap resource service

--- packages/client/src/ts/resources/MindmapResourceService.ts
+++ packages/client/src/ts/resources/MindmapResourceService.ts
@@ -1,20 +1,20 @@
 import { APP, IResource, RESOURCE, ResourceType } from "..";
-import { BlogUpdate, UpdateResult } from "./interface";
+import { MindmapUpdate, UpdateResult } from "./interface";
 import { ResourceService } from "./ResourceService";
 
 export class MindmapResourceService extends ResourceService {
-  async update(parameters: BlogUpdate): Promise<UpdateResult> {
+  async update(parameters: MindmapUpdate): Promise<UpdateResult> {
     const fixThumb = await this.getThumbnailPath(parameters.thumbnail);
-    const res = await this.http.put<IResource>(`/blog/${parameters.entId}`, {
+    const res = await this.http.put<IResource>(`/mindmap/${parameters.entId}`, {
       trashed: parameters.trashed,
       _id: parameters.entId,
-      title: parameters.name,
+      name: parameters.name,
       thumbnail: fixThumb,
       description: parameters.description,
       visibility: parameters.public ? "PUBLIC" : "OWNER",
-      slug: parameters.public ? parameters.slug : "",
+      /* slug: parameters.public ? parameters.slug : "",
       "publish-type": parameters["publish-type"] || "RESTRAINT",
-      "comment-type": "IMMEDIATE",
+      "comment-type": "IMMEDIATE", */
     });
     this.checkHttpResponse(res);
     return { thumbnail: fixThumb, entId: parameters.entId } as UpdateResult;
@@ -26,20 +26,18 @@ export class MindmapResourceService extends ResourceService {
     return APP.MINDMAP;
   }
   getFormUrl(folderId?: string): string {
-    return folderId
-      ? `/mindmap?folderid=${folderId}#/edit/new`
-      : `/mindmap#/edit/new`;
+    return folderId ? `/mindmap?folderid=${folderId}/new` : `/mindmap/new`;
   }
   getViewUrl(resourceId: string): string {
     return `/mindmap/id/${resourceId}`;
   }
   getPrintUrl(resourceId: string, withComment?: boolean): string {
-    return `/mindmap/print/blog#/print/${resourceId}?comments=${
+    return `/mindmap/print/mindmap#/print/${resourceId}?comments=${
       withComment || true
     }`;
   }
 }
 ResourceService.register(
-  { application: RESOURCE.BLOG, resourceType: RESOURCE.BLOG },
+  { application: RESOURCE.MINDMAP, resourceType: RESOURCE.MINDMAP },
   (context) => new MindmapResourceService(context),
 );
