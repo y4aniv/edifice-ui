f90f4aec7f629c2b8d96d06855c2342c5417dbba
Clement Creusat <clement.creusat@gmail.com>
2023-08-08 12:17:41 +0200

    fix: add dts build

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,52 +1,6 @@
-/* import { build } from "vite";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
-import { visualizer } from "rollup-plugin-visualizer";
-
-// const __dirname = fileURLToPath(new URL("..", import.meta.url));
-
-const config = [
-  {
-    entry: resolve(__dirname, "./src/ts/index.cjs.ts"),
-    fileName: () => `ode-ts-client.js`,
-    formats: ["umd"],
-    name: "ode-ts-client",
-  },
-  {
-    entry: resolve(__dirname, "./src/ts/index.ts"),
-    fileName: () => `ode-ts-client.mjs`,
-    formats: ["es"],
-  },
-];
-
-config.forEach(async (lib) => {
-  await build({
-    build: {
-      sourcemap: true,
-      outDir: "./dist",
-      lib: {
-        ...lib,
-      },
-      commonjsOptions: {
-        transformMixedEsModules: true,
-        exclude: ["node_modules"],
-      },
-      rollupOptions: {
-        output: {
-          globals: {
-            rxjs: "RxJS",
-          },
-        },
-      },
-      emptyOutDir: true,
-    },
-    plugins: [visualizer(), dts()],
-  });
-});
- */
-
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
@@ -59,4 +13,5 @@ export default defineConfig({
       formats: ["cjs", "es"],
     },
   },
+  plugins: [dts()],
 });
