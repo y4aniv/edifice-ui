2857118d5c8b90f86603ddcced1dbfa4a2bdf117
Clement Creusat <clement.creusat@gmail.com>
2023-09-25 11:26:23 +0200

    fix: textarea error

--- packages/react/src/components/Textarea/TextArea.stories.tsx
+++ packages/react/src/components/Textarea/TextArea.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { StoryObj } from "@storybook/react";
 
 import TextArea, { TextAreaProps } from "./TextArea";
 import { Ref, useEffect, useRef } from "react";
