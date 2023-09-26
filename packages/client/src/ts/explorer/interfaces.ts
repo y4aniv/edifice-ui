54437ca1863bf83038b576f2faaf64d7b664c4a5
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2023-09-26 13:04:18 +0200

    fix: #Wb2-1126, add search config

--- packages/client/src/ts/explorer/interfaces.ts
+++ packages/client/src/ts/explorer/interfaces.ts
@@ -325,6 +325,7 @@ export interface ISearchResults {
   folders: IFolder[];
   pagination: IPagination;
   resources: IResource[];
+  searchConfig?: {minLength:number}
 }
 //-------------------------------------
 export interface IContext extends ISearchResults {
