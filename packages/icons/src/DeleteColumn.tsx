8208a98578272037d31fb0d730466338a0e36abf
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-10-03 15:57:09 +0200

    feat: #WB-2033, new icons for tiptap table toolbar

---
+++ packages/icons/src/DeleteColumn.tsx
@@ -0,0 +1,30 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgDeleteColumn = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3 4.222C3 3.547 3.547 3 4.222 3H6v3a1 1 0 0 0 2 0V3h11.778C20.453 3 21 3.547 21 4.222v15.556c0 .675-.547 1.222-1.222 1.222H8v-3a1 1 0 1 0-2 0v3H4.222A1.222 1.222 0 0 1 3 19.778V4.222ZM6.978 23H4.222A3.222 3.222 0 0 1 1 19.778V4.222A3.222 3.222 0 0 1 4.222 1H19.778A3.222 3.222 0 0 1 23 4.222v15.556A3.222 3.222 0 0 1 19.778 23h-12.8ZM7 9a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0v-4a1 1 0 0 1 1-1Zm4.318-.182a1 1 0 0 1 1.414 0l1.768 1.768 1.767-1.768a1 1 0 1 1 1.415 1.414L15.914 12l1.768 1.768a1 1 0 1 1-1.414 1.414L14.5 13.414l-1.768 1.768a1 1 0 0 1-1.414-1.414L13.086 12l-1.768-1.768a1 1 0 0 1 0-1.414Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgDeleteColumn;
---
+++ packages/icons/src/DeleteRow.tsx
@@ -0,0 +1,30 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgDeleteRow = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3 4.222C3 3.547 3.547 3 4.222 3h15.556C20.453 3 21 3.547 21 4.222V6h-3a1 1 0 1 0 0 2h3v11.778c0 .675-.547 1.222-1.222 1.222H4.222A1.222 1.222 0 0 1 3 19.778V8h3a1 1 0 0 0 0-2H3V4.222Zm20 0v15.556A3.222 3.222 0 0 1 19.778 23H4.222A3.222 3.222 0 0 1 1 19.778V4.222A3.222 3.222 0 0 1 4.222 1h15.556A3.222 3.222 0 0 1 23 4.222Zm-12.768 7.096a1 1 0 1 0-1.414 1.414l1.768 1.768-1.768 1.767a1 1 0 1 0 1.414 1.415L12 15.914l1.768 1.768a1 1 0 0 0 1.414-1.414L13.414 14.5l1.768-1.768a1 1 0 1 0-1.415-1.414L12 13.086l-1.768-1.768ZM10 6a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgDeleteRow;
