ccdfb40a1ec7ea28761d6b8d61e38b9ac899c0a9
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-10-20 08:44:08 +0200

    fix: #WB-2215, removed legacy code

--- packages/client/src/ts/legacy/entcore.d.ts
+++
@@ -1,7 +0,0 @@
-import { IBehaviours } from "./Behaviours";
-
-declare interface Entcore {
-  Behaviours: IBehaviours;
-}
-export declare var entcore: Entcore;
-export declare var newLogin: boolean;
