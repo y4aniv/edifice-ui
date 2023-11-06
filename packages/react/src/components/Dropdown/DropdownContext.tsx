4a7f7ebce92f205efc81b59a4ef07f49de6b15e6
Clement Creusat <clement.creusat@gmail.com>
2023-11-06 13:30:11 +0100

    fix: missing commits

--- packages/react/src/components/Dropdown/DropdownContext.tsx
+++ packages/react/src/components/Dropdown/DropdownContext.tsx
@@ -4,7 +4,7 @@ import { UseDropdownProps } from "./hooks/useDropdown";
 
 type OmittedProps = Omit<
   UseDropdownProps,
-  "triggerRef" | "menuRef" | "customTriggerProps" | "setVisible"
+  "triggerRef" | "menuRef" | "setVisible"
 >;
 export interface DropdownContextProps extends OmittedProps {
   block?: boolean;
