30fef4fb38d007939ecf6d53369c1bd54e38a113
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-11-09 15:57:51 +0100

    fix(behaviours): add some behaviours

---
+++ packages/client/src/ts/resources/behaviours/CollaborativewallBehaviour.ts
@@ -0,0 +1,47 @@
+import { IResource } from "../interface";
+import { AbstractBehaviourService } from "./AbstractBehaviourService";
+
+type CollaborativewallData = {
+  _id: string;
+  icon: string;
+  name: string;
+  description: string;
+  background: string;
+  owner: {
+    userId: string;
+    displayName: string;
+  };
+  created: {
+    $date: number;
+  };
+  modified: {
+    $date: number;
+  };
+  shared?: [];
+  nbnotes: number;
+};
+
+export class CollaborativewallBehaviour extends AbstractBehaviourService {
+  APP = "collaborativewall";
+  RESOURCE = "collaborativewall";
+
+  async loadResources() {
+    const walls = await this.httpGet<CollaborativewallData[]>(
+      "/collaborativewall/list/all",
+    );
+    return walls.map((data) =>
+      this.dataToResource({
+        title: data.name,
+        ownerName: data.owner.displayName,
+        owner: data.owner.userId,
+        icon: data.icon
+          ? data.icon
+          : "/img/illustrations/collaborative-wall-default.png",
+        path: "/collaborativewall#/view/" + data._id,
+        _id: data._id,
+        shared: data.shared && data.shared.length >= 0 ? true : false,
+        modified: data.modified,
+      }),
+    );
+  }
+}
---
+++ packages/client/src/ts/resources/behaviours/PagesBehaviour.ts
@@ -0,0 +1,74 @@
+import { IResource } from "../interface";
+import { AbstractBehaviourService } from "./AbstractBehaviourService";
+
+type PagesData = {
+  _id: string;
+  title: string;
+  thumbnail?: string;
+  pages?: Array<{
+    title: string;
+    titleLink: string;
+    //    href: string;
+    //    rows: [];
+    published: boolean;
+    owner: string;
+  }>;
+  owner: {
+    userId: string;
+    displayName: string;
+  };
+  created: {
+    $date: number;
+  };
+  modified: {
+    $date: number;
+  };
+  icon: string;
+  description: string;
+  landingPage: string;
+  shared?: [];
+};
+
+export class PagesBehaviour extends AbstractBehaviourService {
+  APP = "pages";
+  RESOURCE = "pages";
+
+  async loadResources() {
+    const websites = await this.httpGet<PagesData[]>("/pages/list/all");
+    const pages: Array<IResource> = [];
+    websites.forEach((website) => {
+      const icon = website.thumbnail
+        ? website.thumbnail + "?thumbnail=48x48"
+        : "/img/illustrations/pages.svg";
+
+      pages.push(
+        this.dataToResource({
+          title: website.title,
+          owner: website.owner.userId,
+          ownerName: website.owner.displayName,
+          icon: icon,
+          path: "/pages#/website/" + website._id,
+          _id: website._id,
+          shared: typeof website.shared !== "undefined",
+          modified: website.modified,
+        }),
+      );
+
+      website.pages?.forEach((page) => {
+        pages.push(
+          this.dataToResource({
+            title: page.title,
+            owner: website.owner.userId,
+            ownerName: website.owner.displayName,
+            icon: icon,
+            path: "/pages#/website/" + website._id + "/" + page.titleLink,
+            _id: website._id + "/" + page.titleLink,
+            shared: typeof website.shared !== "undefined",
+            modified: website.modified,
+          }),
+        );
+      });
+    });
+    return pages;
+  }
+}
