29f44735e987a07ce76f691dfbe10001d04e1e70
Romu-C <romuald.coulaud@gmail.com>
2024-01-17 17:54:43 +0100

    fix: #WB-2309, add maths i18n keys

--- packages/editor/src/components/Editor/MathsModal.tsx
+++ packages/editor/src/components/Editor/MathsModal.tsx
@@ -67,17 +67,17 @@ const MathsModal = ({ isOpen, onSuccess, onCancel }: ModalProps) => {
   return createPortal(
     <Modal id="MathsModal" isOpen={isOpen} onModalClose={handleOnCancel}>
       <Modal.Header onModalClose={handleOnCancel}>
-        {t("Formule mathématique")}
+        {t("tiptap.maths.title")}
       </Modal.Header>
       <Modal.Subtitle>
-        {t("Rédigez vos formules grâce au langage ")}
+        {t("tiptap.maths.subtitle.1")}
         <a
           href={t(
             "https://fr.wikibooks.org/wiki/LaTeX/%C3%89crire_des_math%C3%A9matiques",
           )}
           target="_blank"
         >
-          {t("LaTeX Mathematics")}
+          {t("tiptap.maths.subtitle.2")}
         </a>
       </Modal.Subtitle>
       <Modal.Body>
