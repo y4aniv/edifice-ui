941788cebf514fffc7c0fbb966eafac14cabf6f7
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2024-03-26 17:02:19 +0100

    fix: update image path + remove first slash

--- packages/client/src/ts/resources/services/CollaborativewallResourceService.ts
+++ packages/client/src/ts/resources/services/CollaborativewallResourceService.ts
@@ -11,20 +11,18 @@ const APP = "collaborativewall";
 const RESOURCE = "collaborativewall";
 
 const backgroundImages = [
-  "/img/cloud.png",
-  "/img/default.jpg",
-  "/img/paper.jpg",
-  "/img/wood.jpg",
+  "img/cloud.png",
+  "img/default.jpg",
+  "img/paper.jpg",
+  "img/wood.jpg",
+  "img/blue-topo.png",
+  "img/edifice.png",
+  "img/green-hill.png",
+  "img/orange-hill.png",
+  "img/sport.png",
 ];
 
-const backgroundColors = [
-  "115deg, #E5F5FF 0.32%, #46AFE6 100%",
-  "116.76deg, #FFECEE 0.32%, #FF3A55 99.93%",
-  "116.76deg, #F6ECF9 0.32%, #A348C0 99.93%",
-  "116.76deg, #FFEFE3 0.32%, #FF8D2E 99.93%",
-];
-
-const randomNumber = Math.trunc(Math.random() * (4 - 0) + 0);
+const randomNumber = Math.trunc(Math.random() * (9 - 0) + 0);
 
 export class CollaborativewallResourceService extends ResourceService {
   async create(parameters: CreateParameters): Promise<CreateResult> {
@@ -34,7 +32,7 @@ export class CollaborativewallResourceService extends ResourceService {
       description: parameters.description,
       background: {
         path: backgroundImages[randomNumber],
-        color: backgroundColors[randomNumber],
+        color: "115deg, #E5F5FF 0.32%, #46AFE6 100%",
       },
       icon: thumbnail,
     });
