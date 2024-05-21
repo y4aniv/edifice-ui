1100ce93a400b88747ed23cb8d8d807876fffecf
Clement Creusat <clement.creusat@gmail.com>
2024-05-21 14:54:49 +0200

    fix: #WB2-1702, add number check to convert explorer date

--- packages/react/ui/src/core/useDate/useDate.ts
+++ packages/react/ui/src/core/useDate/useDate.ts
@@ -1,17 +1,15 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 
-import dayjs from "dayjs";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import { useSession } from "../useSession";
-
 import "dayjs/locale/de";
 import "dayjs/locale/es";
-import "dayjs/locale/pt";
 import "dayjs/locale/fr";
 import "dayjs/locale/it";
+import "dayjs/locale/pt";
+import { useOdeClient } from "../OdeClientProvider";
 
 dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
@@ -29,80 +27,90 @@ export type CoreDate = IsoDate | MongoDate;
  */
 export default function useDate() {
   // Current language
-  const [lang, setLang] = useState<string>("fr");
-  const sessionQuery = useSession();
+  const { currentLanguage } = useOdeClient();
 
   /* Utility function */
-  function parseDate(date: string, lang?: string): Dayjs {
-    if (date.length < 11) return dayjs(date, ["YYYY-MM-DD"], lang);
+  const parseDate = useCallback(
+    (date: string, lang?: string): Dayjs => {
+      if (date.length < 11) return dayjs(date, ["YYYY-MM-DD"], lang);
 
-    // Check if the string is exclusively made of digits
-    if (date.split("").findIndex((char) => "0" > char || char > "9") < 0) {
-      // => it should be a number of elapsed milliseconds since 1970-01-01, as a string.
-      return dayjs(Number.parseInt(date));
-    } else {
-      // Otherwise, it should be an ISO 8601 date.
-      let day = dayjs(date);
-      if (!day.isValid()) {
-        // If invalid, try custom parsings (https://day.js.org/docs/en/parse/string-format)
-        day = dayjs(date, ["YYYY-MM-DD HH:mm:ss.SSS"]);
+      // Check if the string is exclusively made of digits
+      if (date.split("").findIndex((char) => "0" > char || char > "9") < 0) {
+        // => it should be a number of elapsed milliseconds since 1970-01-01, as a string.
+        return dayjs(Number.parseInt(date)).locale(currentLanguage as string);
+      } else {
+        // Otherwise, it should be an ISO 8601 date.
+        let day = dayjs(date).locale(currentLanguage as string);
+        if (!day.isValid()) {
+          // If invalid, try custom parsings (https://day.js.org/docs/en/parse/string-format)
+          day = dayjs(date, ["YYYY-MM-DD HH:mm:ss.SSS"]).locale(
+            currentLanguage as string,
+          );
+        }
+        return day;
       }
-      return day;
-    }
-  }
+    },
+    [currentLanguage],
+  );
 
   /** Compute a user-friendly elapsed duration, between now and a date. */
   const fromNow = useCallback(
     (date: CoreDate): string => {
+      console.log("date", { date: typeof date });
       let computedDate: Dayjs = dayjs();
       try {
         if ("undefined" === typeof date) {
           return "";
         } else if ("string" === typeof date) {
-          computedDate = parseDate(date, lang);
+          computedDate = parseDate(date);
+        } else if ("number" === typeof date) {
+          computedDate = dayjs(date).locale(currentLanguage as string);
         } else if ("number" === typeof date.$date) {
-          computedDate = dayjs(new Date(date.$date));
+          computedDate = dayjs(new Date(date.$date)).locale(
+            currentLanguage as string,
+          );
         } else if ("string" === typeof date.$date) {
-          computedDate = parseDate(date.$date, lang);
+          computedDate = parseDate(date.$date);
         }
+
+        console.log({ computedDate: computedDate.fromNow() });
 
         return computedDate.isValid() ? computedDate.fromNow() : "";
       } catch (e) {
         return "";
       }
     },
-    [lang],
+    [currentLanguage, parseDate],
   );
 
   const formatDate = useCallback(
     (date: CoreDate, format?: string): string => {
       let computedDate: Dayjs = dayjs();
+
       try {
         if ("undefined" === typeof date) {
           return "";
         } else if ("string" === typeof date) {
-          computedDate = parseDate(date, lang);
+          computedDate = parseDate(date);
         } else if ("number" === typeof date.$date) {
-          computedDate = dayjs(new Date(date.$date));
+          computedDate = dayjs(new Date(date.$date)).locale(
+            currentLanguage as string,
+          );
         } else if ("string" === typeof date.$date) {
-          computedDate = parseDate(date.$date, lang);
+          computedDate = parseDate(date.$date);
         }
 
         return computedDate.isValid()
-          ? computedDate.locale(lang).format(format || "D MMMM YYYY HH:mm:ss")
+          ? computedDate
+              .locale(currentLanguage as string)
+              .format(format || "D MMMM YYYY HH:mm:ss")
           : "";
       } catch (e) {
         return "";
       }
     },
-    [lang],
+    [currentLanguage, parseDate],
   );
-
-  useEffect(() => {
-    const lang = sessionQuery?.data?.currentLanguage || "fr";
-    setLang(lang);
-    dayjs.locale(lang);
-  }, [sessionQuery?.data, setLang]);
 
   return {
     fromNow,
