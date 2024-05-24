7afac70950eb6ef6bef370b686df2e8f2ebe8bcf
Clement Creusat <clement.creusat@gmail.com>
2024-05-24 10:29:28 +0200

    feat: #INTEG-270, add zendeskGuide and remove help module

--- packages/react/ui/src/portal/Layout/Layout.tsx
+++ packages/react/ui/src/portal/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 
 import Alert from "../../components/Alert/Alert";
 import Button from "../../components/Button/Button";
-import { useOdeTheme } from "../../core";
+import { useOdeTheme, useZendeskGuide } from "../../core";
 import useCookiesConsent from "../../core/useCookiesConsent/useCookiesConsent";
 import Header from "../Header/Header";
 
@@ -27,6 +27,8 @@ const Layout = ({ children, headless = false, ...restProps }: LayoutProps) => {
     handleConsultCookies,
     handleCloseCookiesConsent,
   } = useCookiesConsent();
+
+  useZendeskGuide();
 
   const classes = clsx("d-flex flex-column bg-white", {
     "container-fluid": !headless,
@@ -57,7 +59,6 @@ const Layout = ({ children, headless = false, ...restProps }: LayoutProps) => {
 
   const renderToaster = (
     <Toaster
-      containerClassName="toaster-container"
       toastOptions={{
         position: "top-right",
       }}
