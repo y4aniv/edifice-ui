4f10f5afcdb1453119592443c1192e2c19f2146c
Yaniv Douieb <yanivdouieb@icloud.com>
2024-07-12 12:14:01 +0200

    feat: Change from single-choice to multiple-choice and add functionality

--- packages/react/editor/src/components/Renderer/QcmRenderer.tsx
+++ packages/react/editor/src/components/Renderer/QcmRenderer.tsx
@@ -1,69 +1,209 @@
-import { Button, Card } from "@edifice-ui/react";
+import { Button, Card, Checkbox } from "@edifice-ui/react";
 import { Editor, NodeViewWrapper } from "@tiptap/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 const QcmRenderer = (props: { editor: Editor; [x: string]: any }) => {
-  const [selectedAnswers, setSelectedAnswers] = useState<{
-    [key: number]: number | null;
-  }>({});
-  const [disabled, setDisabled] = useState<{ [key: number]: boolean }>({});
-
-  const handleSelect = (qcmIndex: number, answerIndex: number) => {
-    setSelectedAnswers((prev) => ({ ...prev, [qcmIndex]: answerIndex }));
-    setDisabled((prev) => ({ ...prev, [qcmIndex]: true }));
+  const PropsJson = props.node.attrs.json as {
+    qcmId: string;
+    qcmTitle: string;
+    qcmEndMessage?: string;
+    qcmData: {
+      question: string;
+      options: string[];
+      correctIndex: number[];
+      explanation?: string;
+    }[];
+    qcmSettings: {
+      showScore: boolean;
+      showCorrectAnswers: boolean;
+      showPeerAnswers: boolean;
+    };
   };
+
+  const [checkBoxes, setCheckBoxes] = useState<{
+    [key: number]: { [key: number]: boolean };
+  }>({});
+  const [submitted, setSubmitted] = useState<{ [key: number]: boolean }>({});
+  const [correctAnswersVisible, setCorrectAnswersVisible] = useState<{
+    [key: number]: boolean;
+  }>({});
+  const [incorrectSelections, setIncorrectSelections] = useState<{
+    [key: number]: { [key: number]: boolean };
+  }>({});
+
+  const [usageData, setUsageData] = useState<{
+    [key: number]: { [key: number]: number };
+  }>({});
+
+  useEffect(() => {
+    const storedData = localStorage.getItem(
+      `TipTapQCM-Usage-${PropsJson.qcmId}`,
+    );
+    if (storedData) {
+      setUsageData(JSON.parse(storedData));
+    }
+  }, [PropsJson.qcmId]);
+
+  const handleCheckboxChange = (qcmIndex: number, optionIndex: number) => {
+    setCheckBoxes((prevState) => ({
+      ...prevState,
+      [qcmIndex]: {
+        ...prevState[qcmIndex],
+        [optionIndex]: !prevState[qcmIndex]?.[optionIndex],
+      },
+    }));
+  };
+
+  const handleSubmit = (qcmIndex: number) => () => {
+    const correctAnswers = PropsJson.qcmData[qcmIndex].correctIndex;
+    const userSelections = checkBoxes[qcmIndex] || {};
+
+    const newIncorrectSelections = Object.keys(userSelections).reduce(
+      (acc, key) => {
+        const isSelected = userSelections[Number(key)];
+        const isCorrect = correctAnswers.includes(Number(key));
+        if (isSelected && !isCorrect) {
+          acc[key] = true;
+        }
+        return acc;
+      },
+      {} as { [key: string]: boolean },
+    );
+
+    setIncorrectSelections((prevState) => ({
+      ...prevState,
+      [qcmIndex]: newIncorrectSelections,
+    }));
+
+    setSubmitted((prevState) => ({
+      ...prevState,
+      [qcmIndex]: true,
+    }));
+
+    setCorrectAnswersVisible((prevState) => ({
+      ...prevState,
+      [qcmIndex]: true,
+    }));
+
+    const newUsageData = { ...usageData };
+    Object.keys(userSelections).forEach((key) => {
+      const optionIndex = Number(key);
+      if (!newUsageData[qcmIndex]) {
+        newUsageData[qcmIndex] = {};
+      }
+      if (!newUsageData[qcmIndex][optionIndex]) {
+        newUsageData[qcmIndex][optionIndex] = 0;
+      }
+      newUsageData[qcmIndex][optionIndex] += 1;
+    });
+
+    setUsageData(newUsageData);
+    localStorage.setItem(
+      `TipTapQCM-Usage-${PropsJson.qcmId}`,
+      JSON.stringify(newUsageData),
+    );
+  };
+
+  const calculateScore = () => {
+    const questions = Object.keys(PropsJson.qcmData);
+
+    let validAnswersCount = 0;
+
+    questions.forEach((key) => {
+      const correctAnswers = PropsJson.qcmData[Number(key)].correctIndex;
+
+      const userSelections = checkBoxes[Number(key)] || {};
+      const isValid =
+        correctAnswers.every((correctIndex) => {
+          return userSelections[correctIndex] === true;
+        }) &&
+        correctAnswers.length ===
+          Object.keys(userSelections).filter(
+            (index: any) => userSelections[index],
+          ).length;
+
+      if (isValid) {
+        validAnswersCount += 1;
+      }
+    });
+
+    const totalQuestions = questions.length;
+
+    return `${validAnswersCount}/${totalQuestions}`;
+  };
+
+  const { t } = useTranslation("");
 
   return (
     <NodeViewWrapper>
-      {props.node.attrs.json.map(
+      <Card>
+        <Card.Body>
+          <h2>{PropsJson.qcmTitle}</h2>
+        </Card.Body>
+      </Card>
+      {PropsJson.qcmData.map(
         (
           qcm: {
             question: string;
-            options: string[] | number[];
-            correctIndex: number;
+            options: string[];
+            correctIndex: number[];
             explanation?: string;
           },
           qcmIndex: number,
         ) => (
-          <div data-drag-handle>
-            <Card
-              key={qcmIndex}
-              className="mb-16"
-              isClickable={false}
-              isSelected={false}
-            >
+          <div data-drag-handle key={qcmIndex}>
+            <Card className="mb-16" isClickable={false} isSelected={false}>
               <Card.Body>
                 <Card.Title>
                   <h3>{qcm.question}</h3>
                 </Card.Title>
               </Card.Body>
               <Card.Body>
-                <ul style={{ listStyleType: "decimal" }}>
+                <ul style={{ listStyleType: "none" }}>
                   {qcm.options.map((option, optionIndex) => (
-                    <li key={optionIndex} className="mb-8">
-                      <Button
-                        variant={"ghost"}
-                        onClick={() => handleSelect(qcmIndex, optionIndex)}
-                        disabled={!!disabled[qcmIndex]}
-                        color={"tertiary"}
-                        style={{
-                          color:
-                            selectedAnswers[qcmIndex] != null
-                              ? optionIndex === qcm.correctIndex
-                                ? "var(--edifice-green)"
-                                : selectedAnswers[qcmIndex] === optionIndex
-                                  ? "var(--edifice-red)"
-                                  : ""
-                              : "",
-                        }}
-                      >
-                        {option}
-                      </Button>
+                    <li
+                      key={optionIndex}
+                      className="mb-8"
+                      style={{
+                        color:
+                          PropsJson.qcmSettings.showCorrectAnswers &&
+                          correctAnswersVisible[qcmIndex] &&
+                          qcm.correctIndex.includes(optionIndex)
+                            ? "var(--edifice-green)"
+                            : PropsJson.qcmSettings.showCorrectAnswers &&
+                                incorrectSelections[qcmIndex] &&
+                                incorrectSelections[qcmIndex][optionIndex]
+                              ? "var(--edifice-red)"
+                              : "inherit",
+                      }}
+                    >
+                      <Checkbox
+                        label={`${option} ${
+                          PropsJson.qcmSettings.showPeerAnswers &&
+                          submitted[qcmIndex]
+                            ? `(${usageData[qcmIndex]?.[optionIndex] || 0})`
+                            : ""
+                        }`}
+                        checked={checkBoxes[qcmIndex]?.[optionIndex] || false}
+                        onChange={() =>
+                          handleCheckboxChange(qcmIndex, optionIndex)
+                        }
+                        disabled={submitted[qcmIndex] || false}
+                      />
                     </li>
                   ))}
                 </ul>
               </Card.Body>
-              {selectedAnswers[qcmIndex] != null && qcm.explanation && (
+              <Card.Body>
+                <Button
+                  onClick={handleSubmit(qcmIndex)}
+                  disabled={submitted[qcmIndex] || false}
+                >
+                  {t("tiptap.qcm.validate")}
+                </Button>
+              </Card.Body>
+              {submitted[qcmIndex] && (
                 <Card.Footer>
                   <p>{qcm.explanation}</p>
                 </Card.Footer>
@@ -71,6 +211,18 @@ const QcmRenderer = (props: { editor: Editor; [x: string]: any }) => {
             </Card>
           </div>
         ),
+      )}
+      {Object.keys(submitted).length === PropsJson.qcmData.length && (
+        <Card className="mb-16" isClickable={false} isSelected={false}>
+          <Card.Body>
+            {PropsJson.qcmSettings.showScore && (
+              <h3>{`${t("tiptap.qcm.results")} : ${calculateScore()}`}</h3>
+            )}
+          </Card.Body>
+          <Card.Body>
+            {PropsJson.qcmEndMessage && <p>{PropsJson.qcmEndMessage}</p>}
+          </Card.Body>
+        </Card>
       )}
     </NodeViewWrapper>
   );
